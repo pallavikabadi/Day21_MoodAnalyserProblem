d7ed614ff5b75a551060398f71843a02462abeba
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-04-05 23:17:11 +0530

    added:Handle Exception Using Invalid Exception

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -3,18 +3,26 @@ import junit.framework.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest {
+    //Refactored:
     @Test
-    public void givenMessageWhenSadReturnsSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.moodAnalysis("I am in sad Mood");
+    public void givenMessageWhenMoodSadReturnsSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String mood = moodAnalyser.moodAnalysis();
         Assertions.assertEquals("SAD",mood);
         System.out.println(mood);
     }
     @Test
-    public void givenMessageWhenHappyReturnsHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.moodAnalysis("I am healthy");
-        Assertions.assertEquals("HAPPY",mood);
+    public void givenMessageWhenMoodHappyReturnsSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
+        String mood = moodAnalyser.moodAnalysis();
+        Assertions.assertEquals("SAD",mood);
         System.out.println(mood);
     }
+    //UC2:
+    @Test
+    public void givenMessageWhenNullReturnsHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.moodAnalysis();
+        Assertions.assertEquals("HAPPY",mood);
+        System.out.println(mood);
 }
